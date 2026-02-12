930ee234a222223f18847a483bac8e8f2f5e73c4
kayailker <kaya@Ilkers-MacBook-Pro.local>
2026-02-12 10:11:03 +0300

    (feat) added for google search

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/google2bd328ca7e07186f.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "google2bd328ca7e07186f.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`Server çalışıyor: http://localhost:${PORT}`);
 });
