6dc583d80bf26c9fc811abddb344607e022413c1
kayailker <kaya@Ilkers-MacBook-Pro.local>
2026-02-12 09:27:28 +0300

    (feat) ready for the server.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Static dosyalar (css, js vs)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ana endpoint
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server çalışıyor: http://localhost:${PORT}`);
+});
